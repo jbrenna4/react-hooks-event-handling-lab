feed4c8442a70c74f9736c645872b9ba05d684f8
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-04 12:58:05 -0500

    passed all tests

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+function EyesOnMe (){
+    function handleFocus(){
+        console.log("Good!");
+    }
+
+    function handleBlur(){
+        console.log("Hey! Eyes on me!");
+    }
+
+
+return (
+    <div>
+        <button
+//            type="password"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            >Eyes on me</button>;
+    </div>
+)
+}
+
+export default EyesOnMe;
